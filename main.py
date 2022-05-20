2059b7636afe1b4d03fb63349255b713aa7ba94b
Mike <ms53150@gmail.com>
2022-05-20 18:09:27 -0400

    Allow user to add, update, delete entries in tracker

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 import os
+from datetime import datetime
 
 USERNAME = os.environ.get("USERNAME")
 TOKEN = os.environ.get("TOKEN")
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -19,12 +21,35 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Cycling Graph",
     "unit": "Km",
     "type": "float",
     "color": "sora"
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config)
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+
+
+today = datetime.now()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many kilometers did you cycle today?")
+}
+
+pixela_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+# response = requests.post(url=pixela_creation_endpoint, json=pixel_data, headers=headers)
+
+update_endpoint = f"{pixela_creation_endpoint}/{pixel_data['date']}"
+
+response = requests.put(url=update_endpoint, json=pixel_data, headers=headers)
+
+# delete pixel - entry
+# response = requests.delete(url=update_endpoint, json=pixel_data, headers=headers)
 print(response.text)
